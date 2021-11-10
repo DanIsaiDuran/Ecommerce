e857d12d210c2cb0baee65a07885cb9d31c8c18f
Dan Isai <dan.duran5635@alumnos.udg.mx>
2021-11-10 17:57:16 -0600

    Creacion de seeders para las categorias y subcategorias

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Category;
+
+class CategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'image' => 'categories/' . $this->faker->image('storage/app/public/categories', 640, 480, null, false)
+        ];
+    }
+}
