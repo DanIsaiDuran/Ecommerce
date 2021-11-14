fa6333ed1376507b506648a586fd6b1788ddab01
Dan Isai <dan.duran5635@alumnos.udg.mx>
2021-11-13 22:47:59 -0600

    Diseño de menu y haciendolo responsive

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,5 +36,29 @@
         @stack('modals')
 
         @livewireScripts
+
+        <script>
+            function dropdown(){
+                return{
+                    open: false,
+                    show(){
+                        if(this.open){
+                            //se cierra el menu
+                            this.open = false;
+                            document.getElementsByTagName('html')[0].style.overflow = 'auto'
+                        }else{
+                            //se abre el menu
+                            this.open = true;
+                            document.getElementsByTagName('html')[0].style.overflow = 'hidden'
+                        }
+                    },
+                    close(){
+                        this.open = false;
+                        document.getElementsByTagName('html')[0].style.overflow = 'auto'
+                    }
+                }
+            }
+        </script>
+        
     </body>
 </html>
