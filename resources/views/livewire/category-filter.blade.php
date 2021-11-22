ca598ec52d61eaa3b1b37886197f868c06b2ffa9
Dan Isai <dan.duran5635@alumnos.udg.mx>
2021-11-21 22:51:23 -0600

    Ajustando diseños para hacerlos responsive

--- resources/views/livewire/category-filter.blade.php
+++ resources/views/livewire/category-filter.blade.php
@@ -3,7 +3,7 @@
         <div class=" px-6 py-2 flex justify-between items-center">
             <h1 class=" font-semibold text-gray-700">{{ $category->name }}</h1>
 
-            <div class=" grid grid-cols-2 border border-gray-200 divide-x divide-gray-200 text-gray-500">
+            <div class=" hidden md:block grid grid-cols-2 border border-gray-200 divide-x divide-gray-200 text-gray-500">
                 <i class=" fas fa-border-all p-3 cursor-pointer {{ $view == 'grid' ? 'text-orange-500' : ''}}" wire:click="$set('view', 'grid')"></i>
                 <i class=" fas fa-th-list p-3 cursor-pointer {{ $view == 'list' ? 'text-orange-500' : ''}}" wire:click="$set('view', 'list')"></i>
             </div>
@@ -70,13 +70,13 @@
                 <ul>
                     @foreach ($products as $product)
                     <li class=" bg-white rounded-lg shadow mb-4">
-                        <article class=" flex">
+                        <article class=" md:flex">
                             <figure>
-                                <img class=" h-48 w-56 object-cover object-center" src="{{ Storage::url($product->images->first()->url) }}" alt="">
+                                <img class=" h-48 w-full md:w-56 object-cover object-center" src="{{ Storage::url($product->images->first()->url) }}" alt="">
                             </figure>
 
                             <div class=" flex-1 py-4 px-6 flex flex-col">
-                                <div class=" flex justify-between">
+                                <div class=" lg:flex justify-between">
                                     <div>
                                         <h1 class=" text-lg font-semibold text-gray-700">{{ $product->name }}</h1>
                                         <p class=" font-bold text-gray-700">$ {{ $product->price }}</p>
@@ -110,7 +110,7 @@
                                     </div>
                                 </div>
 
-                                <div class=" mt-auto mb-6">
+                                <div class="mt-4 md:mt-auto mb-6">
                                     <x-danger-enlace href="{{ route('products.show', $product) }}">
                                         Más información
                                     </x-danger-enlace>
